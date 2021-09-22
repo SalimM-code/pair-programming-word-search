92b6607dfd3e9d458937dfd9e0fd3db506a048ef
salimM-code <mustafasalim000@gmail.com>
2021-09-22 17:09:26 -0400

    implemented a word search function
    Co-authored-by: YBYuan007 <YBYuan007@users.noreply.github.com>

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const trans = require("./transpose");
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(""))
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) {return true}
     }
+    let newLetters = trans(letters);
+    // console.log(newLetters);
+    const verticalJoin = newLetters.map(ls => ls.join(""));
+    // console.log(verticalJoin);  // correct line
+    for (i of verticalJoin) {
+        if (i.includes(word))
+            {return true}
+        }
+    return false
 }
-
 module.exports = wordSearch
+
+// we have a horizontal loop
+// we need vertical.
+
+// need to transpose letters
+// once we transpose letters 
+// performe same logic 
